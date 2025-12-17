8d9c03dbd507dd7702e9be7da3f3b6e7660e2b8f
solomon54 <solomontsehay54@gmail.com>
2025-12-17 17:41:02 +0300

    project set up and environment configration

---
+++ alx-rick-and-morty-app/graphql/queries.ts
@@ -0,0 +1,25 @@
+import { gql } from "@apollo/client";
+ 
+export const GET_EPISODES = gql`
+query getEpisodes($page: Int, $filter: FilterEpisode) {
+episodes(page: $page, filter: $filter) {
+    info {
+      count
+      pages
+      next
+      prev
+    }
+    results {
+      id
+      name
+      air_date
+      episode
+      characters {
+        id
+        name
+        image
+      }
+    }
+  }
+}
+`;
